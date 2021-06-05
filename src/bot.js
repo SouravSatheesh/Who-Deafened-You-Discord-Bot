a7c1d9c145ca6a0c0cfe11047f0ca593a5b7d984
Sourav Satheesh <56963819+SouravSatheesh@users.noreply.github.com>
2021-06-05 16:14:39 +0530

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -8,43 +8,42 @@ client.on('ready', () => {
     console.log(`${client.user.tag} has logged in`);
 });
 
-client.on('voiceStateUpdate',async (oldState, newState) => {
+client.on('voiceStateUpdate', async (oldState, newState) => {
     oldState.guild.channels.cache.forEach(channel => {
-        if(channel.name === "bot") botchannel=channel.id;
-        console.log(channel);
+        if (channel.name === "bot") botchannel = channel.id;
     });
-    if(!oldState.serverDeaf && newState.serverDeaf){
+    if (!oldState.serverDeaf && newState.serverDeaf) {
         channel = client.channels.cache.get(botchannel);
         await oldState.guild.fetchAuditLogs()
-        .then((audit) => {
-            const res = audit.entries.first();
-            executor = client.users.cache.get(res.executor.id) 
-            target = client.users.cache.get(res.target.id);
-            channel.send(`${executor} server deafened ${target}`);
-            vsm = oldState.guild.voiceStates.cache;
-            vsm.forEach(state => {
-                if(state.id === res.executor.id){
-                    state.kick();
-                }
-            });
-        })
-        .catch(console.error);
-    } else if(!oldState.serverMute && newState.serverMute){
+            .then((audit) => {
+                const res = audit.entries.first();
+                executor = client.users.cache.get(res.executor.id)
+                target = client.users.cache.get(res.target.id);
+                channel.send(`${executor} server deafened ${target}`);
+                vsm = oldState.guild.voiceStates.cache;
+                vsm.forEach(state => {
+                    if (state.id === res.executor.id) {
+                        state.kick();
+                    }
+                });
+            })
+            .catch(console.error);
+    } else if (!oldState.serverMute && newState.serverMute) {
         channel = client.channels.cache.get(botchannel);
         await oldState.guild.fetchAuditLogs()
-        .then((audit) => {
-            const res = audit.entries.first();
-            executor = client.users.cache.get(res.executor.id) 
-            target = client.users.cache.get(res.target.id);
-            channel.send(`${executor} server muted ${target}`)
-            vsm = oldState.guild.voiceStates.cache;
-            vsm.forEach(state => {
-                if(state.id === res.executor.id){
-                    state.kick();
-                }
-            });
-        })
-        .catch(console.error);
+            .then((audit) => {
+                const res = audit.entries.first();
+                executor = client.users.cache.get(res.executor.id)
+                target = client.users.cache.get(res.target.id);
+                channel.send(`${executor} server muted ${target}`)
+                vsm = oldState.guild.voiceStates.cache;
+                vsm.forEach(state => {
+                    if (state.id === res.executor.id) {
+                        state.kick();
+                    }
+                });
+            })
+            .catch(console.error);
     }
 });
 
